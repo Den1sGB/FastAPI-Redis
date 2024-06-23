ebfcabc411b1f062487cf6691fb02167766d58ae
Den1sGB <denisgomesbomfim@hotmail.com>
2024-06-23 17:48:10 -0300

    Enviando arquivos

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from typing import List
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast_message(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.get("/")
+async def get():
+    return HTMLResponse(content=open("index.html", "r").read())
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast_message(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
